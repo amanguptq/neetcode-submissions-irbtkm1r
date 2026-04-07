bb82c15837095a8c717115df51877b282d08c889
Aman Gupta <47669355+amanguptq@users.noreply.github.com>
2026-04-07 23:22:08 +0530

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int KthSmallest(TreeNode root, int k) {
+
+        List<int> sorted = new List<int>();
+        Inorder(root, sorted);
+        return sorted[k-1]!=null? sorted[k-1]: 0;
+    }
+
+    private void  Inorder(TreeNode n,List<int> s)
+    {
+        if(n == null) return;
+        Inorder(n.left, s);
+        s.Add(n.val);
+        Inorder(n.right,s);
+
+    }
+}
