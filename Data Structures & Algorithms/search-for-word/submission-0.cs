b217ef05cb24727b8b7ef07abbddd9969677cd03
Aman Gupta <47669355+amanguptq@users.noreply.github.com>
2026-05-01 23:35:53 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    private int ROWS, COLS;
+
+    public bool Exist(char[][] board, string word) {
+        ROWS = board.Length;
+        COLS = board[0].Length;
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (Dfs(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private bool Dfs(char[][] board, string word, int r, int c, int i) {
+        if (i == word.Length) {
+            return true;
+        }
+        if (r < 0 || c < 0 || r >= ROWS || c >= COLS ||
+        board[r][c] != word[i] || board[r][c] == '#') {
+            return false;
+        }
+
+        board[r][c] = '#';
+        bool res = Dfs(board, word, r + 1, c, i + 1) ||
+                   Dfs(board, word, r - 1, c, i + 1) ||
+                   Dfs(board, word, r, c + 1, i + 1) ||
+                   Dfs(board, word, r, c - 1, i + 1);
+        board[r][c] = word[i];
+        return res;
+    }
+}
